f043eab5ce20d14abdabf125e90c6ae7e66419f1
Mincong <mincong.zhang@hotmail.com>
2018-08-26 01:55:02 +0100

    Update download.py

--- download.py
+++ download.py
@@ -5,14 +5,16 @@ from six.moves import urllib
 def is_non_zero_file(fpath):  
     return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
 
-def download_data(symbol, start_date):
+def download_data(symbol, start_date, end_date):
     file_path = "../investopedia_data/"+symbol+".csv"
     
     print("Downloading "+symbol)
-    if is_non_zero_file(file_path):
-        print(file_path+" exists!")
-        return
+    #if is_non_zero_file(file_path):
+    #    print(file_path+" exists!")
+    #    return
     
+    #example
+    #https://www.investopedia.com/markets/api/partial/historical/?Symbol=BILI&Type=%20Historical+Prices&Timeframe=Hourly&StartDate=Nov+28%2C+2010&EndDate=Dec+05%2C+2018
     url_prefix = "https://www.investopedia.com/markets/api/partial/historical/?"
     url_symbol = "Symbol="+symbol
     url_type = "&Type=%20Historical+Prices"
@@ -21,8 +23,8 @@ def download_data(symbol, start_date):
     #"%b" get month short name
     #"%B" get month full name
     url_date_lambda = lambda date : date.strftime("%b")+"+"+date.strftime("%d")+"%2C+"+date.strftime("%y")
-    url_start_date="&StartDate="+url_date_lambda(date)
-    url_end_date="&EndDate="+url_date_lambda(datetime.datetime.now())
+    url_start_date="&StartDate="+url_date_lambda(start_date)
+    url_end_date="&EndDate="+url_date_lambda(end_date)
     
     url = url_prefix+url_symbol+url_type+url_timeframe+url_start_date+url_end_date
     
@@ -31,3 +33,4 @@ def download_data(symbol, start_date):
     dataframe_list = pd.read_html(html_content)
     dataframe = dataframe_list[0]
     dataframe.to_csv(file_path)
+    
